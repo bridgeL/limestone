b1c00381f1024846f6ccc6cde920add5672f8f1f
Su <wxlxy316@163.com>
2022-10-09 09:36:05 +0800

    words_v3

--- pageurl.js
+++ pageurl.js
@@ -10,8 +10,9 @@ const page_urls = [
     { info: "[原神]官网视频链接集合", url: "genshin/HD_video" },
     { info: "[英语]连词学习", url: "study/conjunction" },
     { info: "[英语]句子结构染色", url: "study/grammar" },
-    { info: "[英语]单词书（旧版）", url: "study/words" },
-    { info: "[英语]单词书（新版）", url: "study/words-vue3" },
+    { info: "[英语]单词书 v1", url: "study/words" },
+    { info: "[英语]单词书 v2", url: "study/words-vue3" },
+    { info: "[英语]单词书 v3", url: "study/words_v3" },
     { info: "[工具]长安中二时辰", url: "tools/time" },
     { info: "[工具]文字钟", url: "tools/word-clock" },
 ];
